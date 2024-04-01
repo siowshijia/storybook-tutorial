7000564c775b1385027ffd2991587ea170747bdb
siowshijia <siowshijia@gmail.com>
2024-04-01 17:44:49 +0800

    init

---
+++ taskbox/.storybook/main.js
@@ -0,0 +1,19 @@
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  stories: ['../src/components/**/*.stories.js'],
+  staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    '@storybook/addon-a11y',
+  ],
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
